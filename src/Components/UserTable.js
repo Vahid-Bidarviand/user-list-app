532928660f752677efadb5c666917b148b766073
Vahid Bidarviand <vahid.bidarviand@12u12.com>
2022-05-06 15:44:13 +0430

    added button and did more styling changes

--- src/Components/UserTable.js
+++ src/Components/UserTable.js
@@ -1,40 +1,60 @@
 import React from "react";
+import { Col, Row } from "reactstrap";
 
 function UserList() {
   return (
     <div>
       <div class="container">
         <div class="row">
-          <table class="table">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="content-box">
+            <Row className="table-header">
+              <Col xs="10">
+                <span>
+                  This is the list of all your users including their full name,
+                  email, status of their account and their time of registrey.
+                </span>
+              </Col>
+              <Col xs="2">
+                <button type="button" class="btn btn-primary add-user-button">
+                  Add User
+                </button>
+              </Col>
+            </Row>
+            <table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Full Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Time of Registery</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark Otto</td>
+                  <td>Otto@gmail.com</td>
+                  <td>Admin</td>
+                  <td>April 12</td>
+                </tr>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark Otto</td>
+                  <td>Otto@gmail.com</td>
+                  <td>Admin</td>
+                  <td>April 12</td>
+                </tr>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark Otto</td>
+                  <td>Otto@gmail.com</td>
+                  <td>Admin</td>
+                  <td>April 12</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>

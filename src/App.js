09b09496cef777349f9cc74307a0334137c9e94b
Vahid Bidarviand <vahid.bidarviand@12u12.com>
2022-05-06 15:00:21 +0430

    added component directory and made table component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./Components/UserTable";
+import UserList from "./Components/UserTable";
 
 function App() {
   return (
@@ -7,41 +9,7 @@ function App() {
         <h1>We Manage Your Users</h1>
         <p>Manage and edit the list of your users using this app.</p>
       </div>
-
-      <div class="container">
-        <div class="row">
-          <table class="table">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <UserList />
     </div>
   );
 }

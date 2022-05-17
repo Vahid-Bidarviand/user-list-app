2d9f4b784da5ade3a8c0d4c98ae58dc0d2e2e85e
Vahid Bidarviand <vahid.bidarviand@12u12.com>
2022-05-17 13:11:16 +0430

    user modal added

--- src/Components/UserFormModal.js
+++ src/Components/UserFormModal.js
@@ -1,143 +1,162 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Row } from "reactstrap";
 
-function UserForm() {
-  return (
-    <div>
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
-                Enter User Information Here:
-              </h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <Row
-              style={{
-                padding: "15px",
-              }}
-            >
-              <Col>
-                <p>First Name:</p>
-                <p>Last Name:</p>
-                <p>Email:</p>
-                <p>Mobile:</p>
-                <p>Age:</p>
-                <p>Country:</p>
-                <p>City:</p>
-                <p>Account Type:</p>
-              </Col>
-              <Col>
-                <form>
-                  <input
-                    type="text"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <input
-                    type="email"
-                    style={{
-                      marginBottom: "9px",
-                    }}
-                    //   onChange={this.changeFu}
-                    //   value={this.state.content}
-                  ></input>
-                  <div>
-                    <select
-                      style={{
-                        padding: "4px 0",
-                        width: "11.6rem",
-                      }}
-                      placeholder="sss"
-                      //   value="null"
-                    >
-                      <option value="admin">Admin</option>
-                      <option value="user">User</option>
-                    </select>
-                  </div>
-                </form>
-              </Col>
-            </Row>
-            <div
-              class="modal-footer"
-              style={{
-                justifyContent: "center",
-              }}
-            >
-              <button
-                type="button"
-                class="btn btn-primary"
-                disabled={false}
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      status: "",
+      time: "",
+    };
+  }
+
+  onChangeForm(e) {
+    this.setState({
+      firstName: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Enter User Information Here:
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <Row
                 style={{
-                  width: "50%",
+                  padding: "15px",
                 }}
               >
-                Save changes
-              </button>
+                <Col>
+                  <p>First Name:</p>
+                  <p>Last Name:</p>
+                  <p>Email:</p>
+                  <p>Mobile:</p>
+                  <p>Age:</p>
+                  <p>Country:</p>
+                  <p>City:</p>
+                  <p>Account Type:</p>
+                </Col>
+                <Col>
+                  <form>
+                    <input
+                      type="text"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      onChange={this.onChangeForm}
+                      // value={this.state.firstName}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <input
+                      type="email"
+                      style={{
+                        marginBottom: "9px",
+                      }}
+                      //   onChange={this.changeFu}
+                      //   value={this.state.content}
+                    ></input>
+                    <div>
+                      <select
+                        style={{
+                          padding: "4px 0",
+                          width: "11.6rem",
+                        }}
+                        placeholder="sss"
+                        //   value="null"
+                      >
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                      </select>
+                    </div>
+                  </form>
+                </Col>
+              </Row>
+              <div
+                class="modal-footer"
+                style={{
+                  justifyContent: "center",
+                }}
+              >
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  disabled={false}
+                  style={{
+                    width: "50%",
+                  }}
+                >
+                  Save changes
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default UserForm;
--- src/Components/UserTable.js
+++ src/Components/UserTable.js
@@ -1,69 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Row } from "reactstrap";
 
-function UserList() {
-  return (
-    <div>
-      <div class="container">
-        <div class="row">
-          <div className="content-box">
-            <Row className="table-header">
-              <Col xs="10">
-                <span>
-                  This is the list of all your users including their full name,
-                  email, status of their account and their time of registrey.
-                </span>
-              </Col>
-              <Col xs="2">
-                <button
-                  type="button"
-                  class="btn btn-primary add-user-button"
-                  data-toggle="modal"
-                  data-target="#exampleModal"
-                >
-                  Add User
-                </button>
-              </Col>
-            </Row>
-            <table class="table">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Full Name</th>
-                  <th scope="col">Email</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Time of Registery</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark Otto</td>
-                  <td>Otto@gmail.com</td>
-                  <td>Admin</td>
-                  <td>April 12</td>
-                </tr>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark Otto</td>
-                  <td>Otto@gmail.com</td>
-                  <td>Admin</td>
-                  <td>April 12</td>
-                </tr>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark Otto</td>
-                  <td>Otto@gmail.com</td>
-                  <td>Admin</td>
-                  <td>April 12</td>
-                </tr>
-              </tbody>
-            </table>
+class UserList extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        fullName: "Study SSS",
+        email: "test@gmail.com",
+        status: "admin",
+        time: "April 10, 2022",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <div class="container">
+          <div class="row">
+            <div className="content-box">
+              <Row className="table-header">
+                <Col xs="10">
+                  <span>
+                    This is the list of all your users including their full
+                    name, email, status of their account and their time of
+                    registrey.
+                  </span>
+                </Col>
+                <Col xs="2">
+                  <button
+                    type="button"
+                    class="btn btn-primary add-user-button"
+                    data-toggle="modal"
+                    data-target="#exampleModal"
+                  >
+                    Add User
+                  </button>
+                </Col>
+              </Row>
+              <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Time of Registery</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>{this.state.items.fullName}</td>
+                    <td>Otto@gmail.com</td>
+                    <td>Admin</td>
+                    <td>April 12</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default UserList;

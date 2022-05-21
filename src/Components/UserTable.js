d21dcb1c77f863d41a36824aa79692b191a1a972
Vahid Bidarviand <vahid.bidarviand@12u12.com>
2022-05-21 15:04:01 +0430

    getting and showing data from local storage

--- src/Components/UserTable.js
+++ src/Components/UserTable.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import { Col, Row } from "reactstrap";
 
 class UserList extends Component {
-  state = {
-    items: [
-      {
-        id: 1,
-        fullName: "Study SSS",
-        email: "test@gmail.com",
-        status: "admin",
-        time: "April 10, 2022",
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+  componentDidMount() {
+    let getUserList = JSON.parse(localStorage.getItem("userList"));
+    this.setState({
+      items: getUserList,
+    });
+  }
   render() {
     return (
       <div>
@@ -21,6 +22,7 @@ class UserList extends Component {
             <div className="content-box">
               <Row className="table-header">
                 <Col xs="10">
+                  {console.log("ss", this.state.items)}
                   <span>
                     This is the list of all your users including their full
                     name, email, status of their account and their time of
@@ -44,18 +46,31 @@ class UserList extends Component {
                     <th scope="col">#</th>
                     <th scope="col">Full Name</th>
                     <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">City</th>
                     <th scope="col">Status</th>
-                    <th scope="col">Time of Registery</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>{this.state.items.fullName}</td>
-                    <td>Otto@gmail.com</td>
-                    <td>Admin</td>
-                    <td>April 12</td>
-                  </tr>
+                  {this.state.items &&
+                    this.state.items.length > 0 &&
+                    this.state.items.map((user, index) => {
+                      // console.log("ss", user);
+                      return (
+                        <tr key={index}>
+                          <th scope="row">{index + 1}</th>
+                          <td>{user.fullName}</td>
+                          <td>{user.email}</td>
+                          <td>{user.mobile}</td>
+                          <td>{user.age}</td>
+                          <td>{user.country}</td>
+                          <td>{user.city}</td>
+                          <td>{user.status}</td>
+                        </tr>
+                      );
+                    })}
                 </tbody>
               </table>
             </div>
